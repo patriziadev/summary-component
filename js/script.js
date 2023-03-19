a31e40b345a317ed5973ddf1d4777f2e9c44243e
ZaBo <patrizia@zaio.it>
2023-03-19 20:10:09 +0100

    chore: set the result part and fix the responsivity

--- js/script.js
+++ js/script.js
@@ -1,17 +1,40 @@
 const summaryItem = document.getElementById("item");
+const score = document.getElementById("score");
+let dataFetched = [];
 
-fetch("./data.json")
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data);
-        itemToAdd = "";
-        for (let i = 0; i < data.length; i++) {
-            let category = data[i].category;
-            let icon = data[i].icon;
-            let score = data[i].score;
-            itemToAdd += `<div 
+const scoreCalculation = function (reaction, memory, verbal, visual) {};
+
+const fetchData = function () {
+    return fetch("./data.json")
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            dataFetched = data;
+            getResults(dataFetched);
+            completeSummary(dataFetched);
+        })
+        .catch((error) => console.log(error));
+};
+
+const getResults = function (data) {
+    scoresArray = data.map((value) => value.score);
+    averageResult = Math.floor(
+        scoresArray.reduce((a, b) => a + b) / scoresArray.length
+    );
+    score.innerHTML = averageResult;
+};
+
+const completeSummary = function (data) {
+    itemToAdd = "";
+    for (let i = 0; i < data.length; i++) {
+        let category = data[i].category;
+        let icon = data[i].icon;
+        let score = data[i].score;
+        itemToAdd += `<div 
                 class="category ${category.toLowerCase()}"> 
                     <div class="${category.toLowerCase()}"><img src="${icon}" alt="${category}"> ${category}</div><div class="score"><strong>${score}</strong> / 100</div></div>`;
-        }
-        summaryItem.innerHTML = itemToAdd;
-    });
+    }
+    summaryItem.innerHTML = itemToAdd;
+};
+
+fetchData();
